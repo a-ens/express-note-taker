f9de99b0af5628267c061596010ca585bf090658
aenz <andrewjensor@gmail.com>
2022-11-27 00:34:51 -0800

    changed port to 8080 to match heroku

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ app.get('/', (req, res) => {
     res.send("Hello World!");
   });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, './public/index.html'));
+   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/api/notes', (req, res) => {
