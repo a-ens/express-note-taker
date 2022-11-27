9953b51d935c67d0824a5ecc7613605497322231
aenz <andrewjensor@gmail.com>
2022-11-26 23:38:36 -0800

    fixed missing app.use

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/notes.html'))
 });
