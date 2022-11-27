cfc295a5f3e44609c7347d6d4914b395b533aa32
aenz <andrewjensor@gmail.com>
2022-11-27 15:46:24 -0800

    trying a different app.use express.static

--- server.js
+++ server.js
@@ -5,44 +5,44 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
-  });
+  res.send("Hello World!");
+});
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public'));
 app.use(express.json());
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.send(path.join(__dirname, '/public/notes.html'))
 });
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      var notes = JSON.parse(data);
-      res.json(notes);
-    });
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    res.json(notes);
   });
-  
-  app.post('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      var notes = JSON.parse(data);
-      let userNote = req.body;
-      userNote.id = Math.floor(Math.random() * 5000);
-      notes.push(userNote);
-    fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
-        res.json(userNote);
-    });
-    }); 
-  });
+});
 
-  app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
+app.post('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    let userNote = req.body;
+    userNote.id = Math.floor(Math.random() * 5000);
+    notes.push(userNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+      res.json(userNote);
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
 });
