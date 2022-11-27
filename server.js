14a33e7eed266e1ab7587c793a6d461a9924be48
aenz <andrewjensor@gmail.com>
2022-11-26 23:18:34 -0800

    POST Route

--- server.js
+++ server.js
@@ -22,3 +22,15 @@ app.get('/api/notes', (req, res) => {
     });
   });
   
+  app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      var notes = JSON.parse(data);
+      let userNote = req.body;
+      userNote.id = Math.floor(Math.random() * 5000);
+      notes.push(userNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+        res.json(userNote);
+    });
+    }); 
+  });
