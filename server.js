090a88c87d007ba7547b7ff2bc467c72895e3d7f
aenz <andrewjensor@gmail.com>
2022-11-27 18:16:59 -0800

    trying to fix note rendering

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -14,17 +14,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/notes.html'))
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.get('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
-    var notes = JSON.parse(data);
+    let notes = JSON.parse(data);
     res.json(notes);
   });
 });
@@ -36,7 +36,7 @@ app.get('api/notes/:id', (req, res) =>{
 app.post('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
-    var notes = JSON.parse(data);
+    let notes = JSON.parse(data);
     let userNote = req.body;
     userNote.id = Math.floor(Math.random() * 5000);
     notes.push(userNote);
