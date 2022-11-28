3856a3f55b4f35a07af28a9fce591d5df14ba66c
aenz <andrewjensor@gmail.com>
2022-11-27 18:32:25 -0800

    moved POST route above GET

--- server.js
+++ server.js
@@ -9,43 +9,41 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/notes.html'))
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-app.get('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
-    let notes = JSON.parse(data);
-    res.json(notes);
+    var notes = JSON.parse(data);
+    let userNote = req.body;
+    userNote.id = Math.floor(Math.random() * 5000);
+    notes.push(userNote);
+  fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+      res.json(userNote);
   });
+  }); 
 });
 
 app.get('api/notes/:id', (req, res) =>{
   res.json(notes[req.params.id]);
 });
 
-app.post('/api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
-    let notes = JSON.parse(data);
-    let userNote = req.body;
-    userNote.id = Math.floor(Math.random() * 5000);
-    notes.push(userNote);
-    fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
-      res.json(userNote);
-    });
+    var notes = JSON.parse(data);
+    res.json(notes);
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/notes.html'))
 });
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '/index.html'));
+});   
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
+
+
